2bd6dce17c18ea75bbc29cf050976417254648e1
frderoubaix <francois.deroubaix@outlook.fr>
2022-12-01 21:34:16 +0100

    fix: add react query (optimize query with cache)

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider} from "react-query";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+const queryClient = new QueryClient(
+    {
+        defaultOptions: {
+            queries: {
+                staleTime: 5 * 60 * 1000,
+                retry: false,
+                refetchOnWindowFocus: false
+            }
+        }
+    }
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <Router>
+            <Routes>
+                <Route path='/' element={<App />}/>
+                {/* add here Route*/}
+            </Routes>
+        </Router>
+        {process.env.NODE_ENV !== "production" ? <ReactQueryDevtools /> : null}
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
