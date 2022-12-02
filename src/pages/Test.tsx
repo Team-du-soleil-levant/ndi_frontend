25f2ccb32a269c072ab0ad7f6706f448b5de7e3d
frderoubaix <francois.deroubaix@outlook.fr>
2022-12-02 07:14:06 +0100

    fix: Konami code work !

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -4,7 +4,7 @@ import {GeneratedImage} from "../components/openai/GeneratedImage";
 export const AfficherTest = () => {
     return (
         <div className="App">
-            <GeneratedImage prompt="cartoon dog in space" />
+            <GeneratedImage prompt="death star from star wars looking like HIV destroying planet in space" />
         </div>
     )
 }
